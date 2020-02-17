9bd19066ffa98fbfaebb08d105e79281c2cd34b9
KayAllTheWay <kenitagarffus1@gmail.com>
2020-02-17 18:53:57 +0000

    first commit

---
+++ WebApi/Migrations/20200216221812_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MoneyDetails",
+                columns: table => new
+                {
+                    LoanID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BorrowerName = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    FundingAmount = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    RepaymentAmount = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoneyDetails", x => x.LoanID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoneyDetails");
+        }
+    }
+}
